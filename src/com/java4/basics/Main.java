f319c4b91cea71210c01ec8f53d3c1bfa2d673cc
anis <anis.siam@hotmail.com>
2022-12-28 10:03:19 +0200

    first commit

---
+++ src/com/java4/basics/Main.java
@@ -0,0 +1,28 @@
+package com.java4.basics;
+
+public class Main {
+    static int x = 10; //Global Variables
+    static int q;
+    static double i;
+
+    public static void main(String[] args) {
+        int y = 50;//Local  Variables
+       // System.out.println(y);
+
+        y = 20;
+        System.out.println(y);
+
+        int x = 20;
+        //int y = 50; In Correct
+
+        byte z = -128;
+
+        System.out.print("My name is Anis my age ");
+        System.out.println(29);
+
+    }
+
+
+
+
+}
