a720684dc865d6fa261c848633dee21257e92d1d
anis <anis.siam@hotmail.com>
2023-01-18 08:40:12 +0200

    //17/Jan/2023 -Array

--- src/com/java4/basics/Arrays.java
+++ src/com/java4/basics/Arrays.java
@@ -1,21 +1,139 @@
 package com.java4.basics;
 
+import java.util.Scanner;
+
 public class Arrays {
     public static void main(String[] args) {
 
-        String[] names = {"Ali", "sami", "bahaa", "alaa", "mohammed"};
-        System.out.println(names[2]);
+        int[][] bahaa = new int[3][4];
+        int[][] x = {{3, 4, 9}, {8, 9, 7 , 5 }, {9, 5, 4}};
 
-        for(int i = 0 ; i <names.length; i++){
+        for (int i = 0 ; i<x.length;i++){
+            for (int j = 0; j < x[i].length; j++) {
+
+                System.out.print(x[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+       /* System.out.println(bahaa.length);
+        System.out.println(bahaa[0].length);
+        System.out.println(bahaa[0][0]);
+        bahaa[0][0] = 5;
+        System.out.println(bahaa[0][0]);*/
+
+
+
+        /*String names1[] = {"Ali", "Sami", "Soso", "toto" , "Yazan"};
+        String names2[] = {"Foad", "Yazan", "Soso", "fofo"};
+
+        for (int i = 0 ; i<names1.length;i++){
+            //names1[0] => "ALI"  ---i
+            for(int j=0 ; j<names2.length; j++){
+                //names2[0] => "Foad"  ---j
+                if(names1[i].equals(names2[j])){
+                    if(names1[i].equals("Soso")){
+                        System.out.println(names1[i] + " Found");
+
+                        break;
+                    }
+                    //System.out.println(names1[i] + " Found");
+                }
+            }
+        }*/
+
+
+
+
+        /*int myNums[] = {10, 50, 8, 2, 90};
+
+        int sum = 0;
+        int max = myNums[0];
+        int min = myNums[0];
+        for(int i = 0 ; i<myNums.length;i++){
+            sum = sum + myNums[i];
+            if(max<myNums[i]){
+                max = myNums[i];
+            }
+            if(min>myNums[i]){
+                min = myNums[i];
+            }
+            if(myNums[i]==8){
+                System.out.println("Found");
+            }
+        }
+
+        System.out.println("Max is " + max);
+        System.out.println("Min is " + min);
+        System.out.println("Sum is " +  sum);*/
+
+        /*Scanner scanner = new Scanner(System.in);
+        String[] names = new String[5];
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("Plz enter name");
+            names[i] = scanner.nextLine();
+            if (names[i].equalsIgnoreCase("Stop")){
+                System.out.println("Stop");
+                break;
+            }
+        }*/
+
+
+
+
+        /*int myNums[] = new int[5];
+
+        for(int i = 0 ; i<myNums.length;i++){
+            System.out.println("Plz enter num ");
+            myNums[i] = scanner.nextInt();
+        }
+
+        for (int x: myNums) {
+            System.out.print(x + "  ");
+
+        }
+*/
+
+       /* double[] xx = new double[5];
+        for (int i = 0; i < xx.length; i++) {
+            xx[i] = scanner.nextDouble();
+        }*/
+
+        /*Scanner input = new Scanner(System.in);
+        double myarry[] = new double[5];
+        for (double i = 0; i < myarry.length; i++) {
+            System.out.println("ples enter avalue");
+            i = input.nextDouble();
+        }*/
+
+
+//        for (int x : myNums) {
+//            if(x%2==0){
+//                System.out.println(x + " is even" );
+//            }else {
+//                System.out.println(x + " is Odd" );
+//            }
+//        }
+
+
+
+      /*  String[] names = {"Ali", "sami", "bahaa", "alaa", "mohammed"};
+        System.out.println(names[2]);*/
+
+        /*for(int i = 0 ; i <names.length; i++){
            if(names[i].equals("alaa")){
                System.out.println("Found in " + i);
            }
-        }
-      /*  for (String name:names) {
-            System.out.println(name);
-
         }*/
-
+        /*int count = 0;
+        for (String name : names) {
+            if (name.equals("bahaa")) {
+                System.out.println(name + " count " + count);
+            }
+            count++;
+        }
+*/
         /*double myDouble[] = {2.0, 30.0, 55.2, 66.1 , 3.1};
         System.out.println(myDouble.length);
         for(int i = 0 ;i<myDouble.length ; i++){
